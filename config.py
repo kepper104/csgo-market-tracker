04769e2b6876f9946fcbd2316d039a1b7f1e4129
kepper104 <67190416+kepper104@users.noreply.github.com>
2023-04-20 11:40:16 +0300

    Add comment on UTC time

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 TELEGRAM_TOKEN = "<TELEGRAM_BOT_TOKEN>"
 TELEGRAM_CHAT_ID = "<TELEGRAM_CHAT_ID>"
-SENDING_TIME = "05:00"
+SENDING_TIME = "05:00"  # Time is in UTC
 tracked_items = ["Prisma 2 Case", "Clutch Case", "Glove Case"]
